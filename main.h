d0416356b49b0f2779d7b5d872a2a7863ddc6606
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-08-19 04:29:00 -0400

    Okay

---
+++ main.h
@@ -0,0 +1,35 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+// TODO: Dunno what this is
+#define USE_STDPERIPH_DRIVER
+
+/* Includes ------------------------------------------------------------------*/
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+/* Local includes ------------------------------------------------------------------*/
+#include "constants.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
+//#include "usb_bsp.h"
+//#include "usbh_core.h"
+//#include "usbh_usr.h"
+//#include "usbh_midi_core.h"
+//#include "audio.h"
+//#include "timers.h"
+//#include "soundGen.h"
+//#include "my_stm32f4_discovery.h"
+//#include "my_stm32f4_discovery_audio_codec.h"
+//#include "stm32fxxx_it.h"
+//#include "delay.h"
+//#include "chorusFD.h"
+//#include "random.h"
+//#include "drifter.h"
+//#include "resonantFilter.h"
+//#include "adsr.h"
+//#include "sequencer.h"
+#endif /* __MAIN_H */
